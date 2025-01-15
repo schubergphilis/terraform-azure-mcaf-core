456f1ab89bf3381f586d8b199a53b5d544b1d7c5
Ferry Blankendaal <fblankendaal@schubergphilis.com>
2025-01-15 11:23:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "this" {
-  name     = var.resource_group.name
+  name     = var.resource_group_name
   location = var.location
   tags = merge(
     try(var.tags),
